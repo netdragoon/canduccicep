bebd8e8d88c9dc9397bda075dc346673624d69dc
netdragoon <dias.fulvio@gmail.com>
2015-10-15 14:57:29 -0300

    k

---
+++ src/Canducci/Cep/facade/Cep.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Canducci\Cep\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+class Cep extends Facade 
+{
+
+	protected static function getFacadeAccessor() 
+	{ 
+		return 'Cep';
+	}		
+
+}
